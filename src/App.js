6b60e27aa387d7ee086571f5d079348a49b4a1b6
smashflashtech <smash.flash.tech@gmail.com>
2020-12-09 08:38:01 -0800

    sets up router and routes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
+import React from 'react'
 import './App.css';
-import Routes from './config/routes'
+import routes from './config/routes'
 
 function App() {
   return (
     <div className="App">
       <h1>Help me graduate.</h1>
-      <Routes/>
+        { routes }
     </div>
   );
 }
--- src/config/routes.js
+++ src/config/routes.js
@@ -4,13 +4,15 @@ import { Switch, Route, Redirect } from "react-router-dom";
 //Pages
 import ParticipantInfo from "../pages/ParticipantInfo";
 import Dct from "../pages/Dct";
+import Home from "../pages/Home";
 
 //exporting routes
-export default () => (
-  <div>
-  {/* <Switch>
-    <Route exact path='/' component={ParticipantInfo} />
+export default (
+  // <div>
+  <Switch>
+    <Route exact path='/' component={Home} />
+    <Route path='/dct' component={ParticipantInfo} />
     <Route path='/dct' component={Dct} />
-  </Switch> */}
-  </div>
+  </Switch>
+  // </div>
 );
