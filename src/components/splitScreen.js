347702535e758ccb7eba28dd08bead4221d127e9
smashflashtech <smash.flash.tech@gmail.com>
2020-12-12 13:09:11 -0800

    noodling, unable to consistently set props to state

--- src/components/splitScreen.js
+++ src/components/splitScreen.js
@@ -1,17 +1,58 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import "../css/splitScreen.css";
-import SampleDisplay from '../components/SampleDisplay'
-import ComparisonDisplay from '../components/ComparisonDisplay'
+
 
 const SplitScreen = (props) => {
-  console.log (props)
+  const [trials, setTrials] = useState([])
+  const [src, setSrc] = useState(require(`../stimuli/${props.os.imagePath}`).default)
+
+  //these are set on a trial by trial basis
+  const [samplePath, setSamplePath] = useState()
+  const [comparisonArray, setComparisonArray] = useState([])//constructor will set this as jsx
+  //phase trackinig logic
+  const [block, setBlock] = useState(1)
+  const [trial, setTrial] = useState(1)
+  // const [totalTrials] = useState(props.trials.length)
+  const [corrects, setCorrects] = useState(0)
+  const [criteria, setCriteria] = useState() //NEED TO CHECK THIS LATER
+
+  const [clickCounter, setClickCounter] = useState(0)
+  const [i, setI] = useState(0)
+
+
+  // const stateSetup = () => {
+  //   if (props !== undefined) {
+  //     console.log(props.trials)
+  //     setSrc(require(`../stimuli/${props.os.imagePath}`).default)
+  //     setTrials(props.trials)
+  //     setCriteria(12) // THIS NEEDS TO CHANGE LATER
+  //   }
+  // }
+  
+  
+  // useEffect(() => { stateSetup() }, [])
+  
+  //this is where you need to scramble the the trials and send the specific sample and respeective comparison to the components
+  
+  //     setSamplePath(require(`../stimuli/${data.dct.trials[0][0].imagePath}`).default) // THIS IS WRONG THIS ISNT WHERE THE SAMPLE IS ASSIGNED
+  
+
+
+  // console.log("TRIALS?", trials)
+  // console.log("COMPARISON ARRAY?", comparisonArray)
+
+
+
   return (
     <div id="container">
       <div class="top">
-        <SampleDisplay os={props.os} samplePath={props.samplePath}/>
+        <div class="sample-container">
+          <img class="os-sample" src={src} />
+        </div>
       </div>
       <div class="bottom">
-      <ComparisonDisplay/>
+        <div class="comparison-container">
+        </div>
       </div>
     </div>
   )
--- src/pages/Dct.js
+++ src/pages/Dct.js
@@ -3,27 +3,26 @@ import DctModel from '../models/dct'
 import SplitScreen from '../components/SplitScreen'
 
 const Dct = () => {
-  const [instructions, setInstructions] = useState()
   const [os, setOs] = useState()
   const [samplePath, setSamplePath] = useState()
   const [trials, setTrials] = useState()
   const [pretest, setPretest] = useState()
   const [posttest, setPostest] = useState()
 
-  const fetchStimuli=() => {
+  const fetchStimuli =() => {
     DctModel.stimuli().then((data)=>{
-      console.log(data.dct)
+      // console.log(data.dct)
       //constructs the image path string
-      setSamplePath(require(`../stimuli/${data.dct.trials[0][0].imagePath}`).default)
+
 
       //these need to be passed as props
-      setInstructions(data.dct.instructions)
       setOs(data.dct.observingStim)
       setTrials(data.dct.trials)
       setPretest(data.dct.masteryCriterion.preTest.percentage)
       setPostest(data.dct.masteryCriterion.postTest.percentage)
       })
   }
+  
 
   useEffect( () => { fetchStimuli() },[])
 
@@ -32,8 +31,8 @@ const Dct = () => {
     <div>
       {/* { sample !== undefined ? <img src= { sample } /> : "" }
       <img src= {orange} /> */}
-      {console.log(os)}
-      { os !== undefined ? <SplitScreen os={os} samplePath={samplePath}/> : "" } 
+      { console.log(samplePath)}
+      { os !== undefined ? <SplitScreen os={os} trials={trials}/> : "" } 
     </div>
   )
 }
