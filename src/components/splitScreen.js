8e640644f115e35ba56eef7b376806539a6d7ea4
smashflashtech <smash.flash.tech@gmail.com>
2020-12-12 14:31:59 -0800

    formats comparison array

--- src/components/splitScreen.js
+++ src/components/splitScreen.js
@@ -28,14 +28,14 @@ const SplitScreen = (props) => {
   //     setCriteria(12) // THIS NEEDS TO CHANGE LATER
   //   }
   // }
-  
-  
+
+
   // useEffect(() => { stateSetup() }, [])
-  
+
   //this is where you need to scramble the the trials and send the specific sample and respeective comparison to the components
-  
+
   //     setSamplePath(require(`../stimuli/${data.dct.trials[0][0].imagePath}`).default) // THIS IS WRONG THIS ISNT WHERE THE SAMPLE IS ASSIGNED
-  
+
 
 
   // console.log("TRIALS?", trials)
@@ -53,9 +53,9 @@ const SplitScreen = (props) => {
       </div>
       <div class="bottom">
         <div class="comparison-container">
-        <img src={props.trials[0][1]} class={props.trials[0][2]} />
-        <img src={props.trials[0][3]} class={props.trials[0][4]} />
-        <img src={props.trials[0][5]} class={props.trials[0][6]} />
+          <img src={props.trials[0][1]} class={props.trials[0][2]} />
+          <img src={props.trials[0][3]} class={props.trials[0][4]} />
+          <img src={props.trials[0][5]} class={props.trials[0][6]} />
         </div>
       </div>
     </div>
