e8cbfbdb3e2f3d57ca4e98b5cd6e694e2bd964c3
smashflashtech <smash.flash.tech@gmail.com>
2020-12-12 21:59:51 -0800

    adds handle sample and comparison click functions, working

--- src/components/splitScreen.js
+++ src/components/splitScreen.js
@@ -8,27 +8,49 @@ const SplitScreen = (props) => {
   console.log("post this to the db: ", props.block)
   console.log("use this to determine if trials are done: ", props.maxTrials)
   console.log("heres the OS: ", props.os)
+  const [sClick, setSClick] = useState(0)
   const [i, setI] = useState(0)
+  const [trial, setTrial] = useState(1) //tracks what trial we're on and will be used to construct block code
+  const [trialCode, setTrialCode] = useState()
+  
+  const [score, setScore] = useState()
 
+  const handleSampleClick = (e) => {
+    e.preventDefault()
+    setTrialCode(e.target.getAttribute("value"))
+    console.log("HERES THE TRIAL CODE", trialCode)
+    e.target.setAttribute("src", props.trials[i][0])
+    setSClick(sClick + 1)
+    console.log(sClick)
+  }
 
-
+  const handleComparisonClick = (e) => {
+    e.preventDefault()
+    setScore(e.target.getAttribute("value"))
+    console.log("HERES THE SCORE", score)
+    // document.getElementbyId("os-sample").setAttribute("src", props.os)
+    setSClick(0)
+    console.log("CLEAR S CLICKS: ",sClick)
+    setI(i + 1)
+    setTrial(trial + 1)
+  }
 
   return (
     <div id="container">
       <div className="top">
         <div className="sample-container">
           {props.trials.length ?
-            <img className="os-sample" src={props.trials[i][0]} value={props.trials[i][1]} />
+            <img alt="os-sample" id="os-sample" className="os-sample" src={props.os} value={props.trials[i][1]} onClick={handleSampleClick}/>
             : <div/>}
         </div>
       </div>
       <div className="bottom">
         <div className="comparison-container">
-          {props.trials.length ?
+          {props.trials.length && sClick === 2 ?
             <>
-              <img src={props.trials[i][2]} className={props.trials[i][3]} value={props.trials[i][4]} />
-              <img src={props.trials[i][5]} className={props.trials[i][6]} value={props.trials[i][7]} />
-              <img src={props.trials[i][8]} className={props.trials[i][9]} value={props.trials[i][10]} />
+              <img alt={props.trials[i][3]} src={props.trials[i][2]} className={props.trials[i][3]} value={props.trials[i][4]} onClick={handleComparisonClick} />
+              <img alt={props.trials[i][6]} src={props.trials[i][5]} className={props.trials[i][6]} value={props.trials[i][7]} onClick={handleComparisonClick}/>
+              <img alt={props.trials[i][9]} src={props.trials[i][8]} className={props.trials[i][9]} value={props.trials[i][10]} onClick={handleComparisonClick}/>
             </>
             :
             <div/>
