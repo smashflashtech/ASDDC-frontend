091be429756d829c9e0b247a1264c91048bd8930
smashflashtech <smash.flash.tech@gmail.com>
2020-12-13 11:10:09 -0800

    moves setMetUrl constructor outside of model function, removes console logs, removes unused state functions. COMPLETES dct. (except maybe gray screen)

--- src/pages/Dct.js
+++ src/pages/Dct.js
@@ -7,14 +7,14 @@ const Dct = (props) => {
   const [trials, setTrials] = useState([])
 
   const [os, setOs] = useState()
-  const [block, setBlock] = useState(parseInt(props.match.params.block))
-  const [condition, setCondition] = useState()
+  const [block] = useState(parseInt(props.match.params.block))
+  const [condition] = useState()
   const [maxTrials, setMaxTrials] = useState()
   const [criteria, setCriteria] = useState()
   const [metUrl, setMetUrl] = useState()
 
-  const fetchStimuli =() => {
-    DctModel.stimuli().then((data)=>{
+  const fetchStimuli = () => {
+    DctModel.stimuli().then((data) => {
       console.log(data.dct)
       //Sets Observing stimuls
       const tempOs = require(`../stimuli/${data.dct.observingStim.imagePath}`).default
@@ -29,7 +29,7 @@ const Dct = (props) => {
         temp.push(sample)
         const trialCode = data.dct.trials[i][0].trialCode
         temp.push(trialCode)
-        for(let j = 1; j < data.dct.trials[i].length; j++) {
+        for (let j = 1; j < data.dct.trials[i].length; j++) {
           let color = data.dct.trials[i][j].color
           temp.push(color)
           let comparison = require(`../stimuli/${data.dct.trials[i][j].imagePath}`).default
@@ -41,53 +41,44 @@ const Dct = (props) => {
         }
         megaStimulusBank.push(temp)
         for (let k = 0; k < megaStimulusBank.length; k++) {                                       //SHUFFLES THE ARRAY
-          let l = Math.floor(Math.random() * megaStimulusBank.length);  
-          let temp = megaStimulusBank[k]; 
+          let l = Math.floor(Math.random() * megaStimulusBank.length);
+          let temp = megaStimulusBank[k];
           megaStimulusBank[k] = megaStimulusBank[l];
-          megaStimulusBank[l] = temp;   
+          megaStimulusBank[l] = temp;
         }
       }
       setTrials(megaStimulusBank)
-
-    //CONSTRUCT MET AND UNMET URLS
-      //if dyad = 1 and block = 1 move to namts OR dyad = 2 and block = 3 move to namts
-      console.log("WHATS dyadN", dyadN)
-      console.log("WHATS block", block)
-      if (dyadN === 1) {
-        setMetUrl('/instructions/namts/101')
-      } else if (dyadN === 2 && block < 3){
-        setMetUrl(`/dct/pre/${block + 1}`)
-      } else if (dyadN === 2 && block === 3) {
-        setMetUrl('/instructions/namts/101')
-      }
-      console.log("WHATS UP WITH URL",metUrl)
-      //
-
-      //if dyad = 2, and block < 3 create link with block + 1
-
-
     })
+    //CONSTRUCT MET AND UNMET URLS depending on dyad
+    //if dyad = 1 and block = 1 move to namts OR dyad = 2 and block = 3 move to namts
+    if (dyadN === 1) {
+      setMetUrl('/instructions/namts/101')
+    } else if (dyadN === 2 && block < 3) {
+      setMetUrl(`/dct/pre/${block + 1}`)
+    } else if (dyadN === 2 && block === 3) {
+      setMetUrl('/instructions/namts/101')
+    }
   }
-  
 
-  useEffect( () => { fetchStimuli() },[])
+
+  useEffect(() => { fetchStimuli() }, [])
 
   //THIS IS LIKELY GOING TO BE PASS DOWN TO COMPONENTS WHEN YOU WORK IT OUT
   return (
     <div>
 
       { console.log("THIS IS condition", condition)}
-      { console.log( trials)}
-      { trials.length !== undefined ? 
-      <SplitScreen3 
-        os={os} 
-        trials={trials} 
-        set={props.match.params.condition}
-        block={block}
-        maxTrials={maxTrials}
-        criteria={criteria}
-        metUrl={metUrl}
-      /> : "" } 
+      { console.log(trials)}
+      { trials.length !== undefined ?
+        <SplitScreen3
+          os={os}
+          trials={trials}
+          set={props.match.params.condition}
+          block={block}
+          maxTrials={maxTrials}
+          criteria={criteria}
+          metUrl={metUrl}
+        /> : ""}
     </div>
   )
 }
