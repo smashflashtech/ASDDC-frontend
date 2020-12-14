cd4864b248f5ae71c33ae1d1f53ffcfc1eccb847
smashflashtech <smash.flash.tech@gmail.com>
2020-12-14 00:01:34 -0800

    packages stimuli and criteria anad passes as props to child component

--- src/pages/Amts.js
+++ src/pages/Amts.js
@@ -2,39 +2,77 @@ import React, { useState, useEffect } from 'react'
 import SplitScreen2 from '../components/SplitScreen2'
 import AmtsModel from '../models/amts'
 
-const Amts = () => {
-  const [comparison, setComparison] = useState()
-  const [instructions, setInstructions] = useState()
+const Amts = (props) => {
   const [os, setOs] = useState()
-  // const [sample, setSample] = useState()
-  const [trials, setTrials] = useState()
-  // const [pretest, setPretest] = useState() //criteria not yet added to json file on the backend
-  // const [posttest, setPostest] = useState()
+  const [trials, setTrials] = useState([])
+  const [block] = useState(parseInt(props.match.params.block))
+  const [condition] = useState(props.match.params.condition)
+  const [phase] = useState('amts')
+  const [maxTrials, setMaxTrials] = useState()
+  const [criteria, setCriteria] = useState()
+  const [metUrl, setMetUrl] = useState()
+  const [criteriaRequired] = useState("true")
 
-  const fetchStimuli=() => {
-    AmtsModel.stimuli().then((data)=>{
-      
-      //constructs the image path string
-      setComparison(require(`../stimuli/${data.amts.trials[0][1].imagePath}`).default)
+  const fetchStimuli = () => {
+    AmtsModel.stimuli().then((data) => {
+      console.log(data.amts)
+      const tempOs = require(`../stimuli/${data.amts.observingStim.imagePath}`).default
+      setOs(tempOs)
+      setCriteria(data.amts.trials.length)
+      setMaxTrials(data.amts.trials.length)
+      // constructs the trials array for rendering
+      const megaStimulusBank = []
+      for (let i = 0; i < data.amts.trials.length; i++) {
+        const temp = []
+        const sample = require(`../stimuli/${data.amts.trials[i][0].imagePath}`).default
+        temp.push(sample)
+        const trialCode = data.amts.trials[i][0].trialCode
+        temp.push(trialCode)
+        for (let j = 1; j < data.amts.trials[i].length; j++) {
+          let color = data.amts.trials[i][j].color
+          temp.push(color)
+          let comparison = require(`../stimuli/${data.amts.trials[i][j].imagePath}`).default
+          temp.push(comparison)
+          let position = data.amts.trials[i][j].class
+          temp.push(position)
+          let value = data.amts.trials[i][j].value
+          temp.push(value)
+        }
+        megaStimulusBank.push(temp)
+        for (let k = 0; k < megaStimulusBank.length; k++) {                                       //SHUFFLES THE ARRAY
+          let l = Math.floor(Math.random() * megaStimulusBank.length);
+          let temp = megaStimulusBank[k];
+          megaStimulusBank[k] = megaStimulusBank[l];
+          megaStimulusBank[l] = temp;
+        }
+      }
+      setTrials(megaStimulusBank)
+    })
+    //CONSTRUCT MET AND UNMET URLS
 
-      // //these need to be passed as props
-      setInstructions(data.amts.instructions)
-      setOs(data.amts.observingStim)
-      setTrials(data.amts.trials)
-      // setPretest(data.amts.masteryCriterion.preTest.percentage)
-      // setPostest(data.amts.masteryCriterion.postTest.percentage)
-      })
+
+
   }
-  useEffect( () => { fetchStimuli() },[])
-  
+
+  useEffect(() => { fetchStimuli() }, [])
+
   console.log("~~~~~~~~~~~~~~~~~~~~~~~", trials)
   console.log("~~~~~~~~~~~~~~~~~~~~~~~", os)
-  console.log("~~~~~~~~~~~~~~~~~~~~~~~", instructions)
 
   return (
     <div>
-            {/* { comparison !== undefined ? <img src= { comparison } /> : "" } */}
-      <SplitScreen2 />
+      { trials.length !== undefined ?
+        <SplitScreen2
+          os={os}
+          trials={trials}
+          set={condition}
+          block={block}
+          maxTrials={maxTrials}
+          criteria={criteria}
+          metUrl={metUrl}
+          phase={phase}
+          criteriaRequired={criteriaRequired}
+        /> : ""}
     </div>
   )
 }
