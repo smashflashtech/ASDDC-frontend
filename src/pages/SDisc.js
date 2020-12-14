1a0afbe178a7f58ca7d826f1b58124bc7479a2b2
smashflashtech <smash.flash.tech@gmail.com>
2020-12-14 15:48:26 -0800

    retrieves and packages data

--- src/pages/SDisc.js
+++ src/pages/SDisc.js
@@ -1,38 +1,70 @@
 import React, { useState, useEffect } from 'react'
 import SdModel from '../models/sd'
+import SplitScreen2 from '../components/SplitScreen2'
+import Counter from '../components/Counter'
 
 const SDisc = () => {
-  const [comparison, setComparison] = useState()
-  const [instructions, setInstructions] = useState()
+  const [phase] = useState('sdisc')
+  const [count, setCount] = useState(0)
   const [os, setOs] = useState()
-  // const [sample, setSample] = useState()
   const [trials, setTrials] = useState()
-  // const [pretest, setPretest] = useState() //criteria not yet added to json file on the backend
-  // const [posttest, setPostest] = useState()
+  const [maxTrials, setMaxTrials] = useState()
+  const [criteria, setCriteria] = useState()
+  const [criteriaRequired] = useState("true")
 
   const fetchStimuli = () => {
     SdModel.stimuli().then((data) => {
-      console.log(data.sd.trials)
-      //constructs the image path string
-      setComparison(require(`../stimuli/${data.sd.trials[0][1].imagePath}`).default)
-
-      // //these need to be passed as props
-      setInstructions(data.sd.instructions)
-      setOs(data.sd.observingStim)
-      setTrials(data.sd.trials)
-      // setPretest(data.sd.masteryCriterion.preTest.percentage)
-      // setPostest(data.sd.masteryCriterion.postTest.percentage)
+      console.log(data.sd)
+      const tempOs = require(`../stimuli/${data.sd.observingStim.imagePath}`).default
+      setOs(tempOs)
+      setCriteria(data.sd.trials.length)
+      setMaxTrials(data.sd.trials.length)
+      // constructs the trials array for rendering
+      const megaStimulusBank = []
+      for (let i = 0; i < data.sd.trials.length; i++) {
+        const temp = []
+        const trialCode = data.sd.trials[i][0].trialCode
+        temp.push(trialCode)
+        for (let j = 1; j < data.sd.trials[i].length; j++) {
+          let color = data.sd.trials[i][j].color
+          temp.push(color)
+          let comparison = require(`../stimuli/${data.sd.trials[i][j].imagePath}`).default
+          temp.push(comparison)
+          let position = data.sd.trials[i][j].class
+          temp.push(position)
+          let value = data.sd.trials[i][j].value
+          temp.push(value)
+        }
+        megaStimulusBank.push(temp)
+        for (let k = 0; k < megaStimulusBank.length; k++) {                                       //SHUFFLES THE ARRAY
+          let l = Math.floor(Math.random() * megaStimulusBank.length);
+          let temp = megaStimulusBank[k];
+          megaStimulusBank[k] = megaStimulusBank[l];
+          megaStimulusBank[l] = temp;
+        }
+      }
+      setTrials(megaStimulusBank)
     })
   }
   useEffect(() => { fetchStimuli() }, [])
 
   console.log("~~~~~~~~~~~~~~~~~~~~~~~", trials)
   console.log("~~~~~~~~~~~~~~~~~~~~~~~", os)
-  console.log("~~~~~~~~~~~~~~~~~~~~~~~", instructions)
 
   return (
     <div>
-      { comparison !== undefined ? <img src={comparison} /> : ""}
+      <Counter count={count} />
+      <SplitScreen2
+        setCount={setCount}
+        os={os}
+        trials={trials}
+        block={block}
+        maxTrials={maxTrials}
+        criteria={criteria}
+        metUrl={metUrl}
+        phase={phase}
+        criteriaRequired={criteriaRequired}
+      />
     </div>
   )
 }
