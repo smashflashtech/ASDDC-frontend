f5858c75cfc133be84375acb600b88d7405c59ca
smashflashtech <smash.flash.tech@gmail.com>
2020-12-14 23:39:44 -0800

    adds model skeleton

---
+++ src/models/arp.js
@@ -0,0 +1,11 @@
+const url = `http://localhost:5000`
+
+class ArpModel {
+
+  // static stimuli = () => {
+  //   return fetch(`${url}/arp`).then(res => res.json())
+  // }
+
+}
+
+export default ArpModel
