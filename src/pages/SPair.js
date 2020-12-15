691f2877e4e07b095988ba44fe4e406309dcf8ec
smashflashtech <smash.flash.tech@gmail.com>
2020-12-15 00:31:56 -0800

    receives, packages data, and sends as props to splitscreensp

--- src/pages/SPair.js
+++ src/pages/SPair.js
@@ -3,24 +3,50 @@ import SplitScreenSp from '../components/SplitScreenSp'
 import SpModel from '../models/sp'
 import Counter from '../components/Counter'
 
-const SPair = () => {
+const SPair = (props) => {
   const [count, setCount] = useState(0)
+  const [phase] = useState('spair')
+  const [block] = useState(parseInt(props.match.params.block))
   const [os, setOs] = useState()
-  // const [sample, setSample] = useState()
-  const [trials, setTrials] = useState()
-  // const [pretest, setPretest] = useState() //criteria not yet added to json file on the backend
-  // const [posttest, setPostest] = useState()
+  const [trials, setTrials] = useState([])
+  const [maxTrials, setMaxTrials] = useState()
+  const [criteria, setCriteria] = useState()
+  const [criteriaRequired] = useState("true")
+  const [metUrl, setMetUrl] = useState()
+  const [notMetUrl, setNotMetUrl] = useState()
 
   const fetchStimuli = () => {
     SpModel.stimuli().then((data) => {
       console.log(data.sp)
-
+      const tempOs = require(`../stimuli/${data.sp.observingStim.imagePath}`).default
+      setOs(tempOs)
+      setCriteria(data.sp.trials.length)
+      setMaxTrials(data.sp.trials.length)
+      // constructs the trials array for rendering
+      let megaStimulusBank = []
+      for (let i = 0; i < data.sp.trials.length; i++) {
+        let temp = []
+        let trialCode = data.sp.trials[i].trialCode
+        temp.push(trialCode)
+        let comparison = require(`../stimuli/${data.sp.trials[i].imagePath}`).default
+        temp.push(comparison)
+        let value = data.sp.trials[i].value
+        temp.push(value)
+        megaStimulusBank.push(temp)
+      }
+      for (let k = 0; k < megaStimulusBank.length; k++) {                                       //SHUFFLES THE ARRAY
+        let l = Math.floor(Math.random() * megaStimulusBank.length);
+        let temp = megaStimulusBank[k];
+        megaStimulusBank[k] = megaStimulusBank[l];
+        megaStimulusBank[l] = temp;
+      }
+      setTrials(megaStimulusBank)
     })
+    localStorage.setItem('spair', block)
+    setMetUrl('/sdiscs/1')
+    setNotMetUrl(`/spair/${block + 1}`)
   }
   useEffect(() => { fetchStimuli() }, [])
-
-  console.log("~~~~~~~~~~~~~~~~~~~~~~~", trials)
-  console.log("~~~~~~~~~~~~~~~~~~~~~~~", os)
 
 
   return (
@@ -29,7 +55,16 @@ const SPair = () => {
       <SplitScreenSp
         setCount={setCount}
         count={count}
-
+        condition={"tr"}
+        os={os}
+        trials={trials}
+        block={block}
+        maxTrials={maxTrials}
+        criteria={criteria}
+        phase={phase}
+        criteriaRequired={criteriaRequired}
+        metUrl={metUrl}
+        notMetUrl={notMetUrl}
       />
     </div>
   )
