84776b1cdbe4b4325a04e58ea9f529ab3a64d2fc
smashflashtech <smash.flash.tech@gmail.com>
2020-12-15 01:06:03 -0800

    adds handle click for sample and comparison, counter is working

--- src/components/SplitScreenSp.js
+++ src/components/SplitScreenSp.js
@@ -2,14 +2,78 @@ import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
 import '../css/splitScreenSp.css'
 
-const SplitScreenSp = () => {
+const SplitScreenSp = (props) => {
+  console.log("are we in bidness?", props.trials)
+  console.log("dont forget you have access to count and setCount")
+  const [sClick, setSClick] = useState(0)
+  const [i, setI] = useState(0)
+  const [trial, setTrial] = useState(1) //tracks what trial we're on and will be used to construct block code  const [blockCode, setBlockCode] = useState()
+  const [trialCode, setTrialCode] = useState()
+  const [blockCode, setBlockCode] = useState()
+  const [color, setColor] = useState()
+  const [value, setValue] = useState()
+  
+  const handleSampleClick = (e) => {
+    e.preventDefault()
+    //grabs the trial code
+    setTrialCode(e.target.getAttribute('value'))
+    //constructs the block code and stores in state
+    setBlockCode(`${props.phase}-${props.block}-${trial}`)
+    //changes os image to invisible
+    e.target.setAttribute('src', '')
+    e.target.setAttribute('alt', '')
+    //adds to click count which will determine if comparisons should display
+    setSClick(sClick + 1)
+  }
+
+  const handleComparisonClick = (e) => {
+    e.preventDefault()
+    //counts trial
+    setTrial(trial + 1)
+    //info on how the participant did
+    let color = e.target.getAttribute('alt')
+    setColor(color)
+    let selectedValue = e.target.getAttribute('value')
+    setValue(selectedValue)
+    if (color === "purple") {
+      console.log("YESSS")
+      props.setCount(props.count + 5)
+    } else if (color === "orange") {
+      console.log("NOOO")
+      props.setCount(props.count - 5)
+    }
+    console.log(props.count)
+    //resets the os-sample stimulus, clicks to 0
+    document.getElementById('os-sample').setAttribute('src', props.os)
+    document.getElementById('os-sample').setAttribute('alt', 'os-sample')
+    setSClick(0)
+    //adds to iterator
+    setI(i + 1)
+
+
+    //NEED TO ADD POST to the Database
+    //where PARTICIPANT ID (stored in local storate)
+    //and trialCode (create a join)
+    //need post to db
+    //trial code
+    //block code
+    //color
+    //value
+
+
+  }
+
+
+
+  
+
   return (
     <div id="container-sp">
       <div className="top-sp">
         <div className="sample-container-sp">
-          {/* {props.trials.length && trial <= props.maxTrials ?
+          {props.trials.length && trial <= props.maxTrials ?
             <img alt="os-sample" id="os-sample" className="os-sample" src={props.os} value={props.trials[i][0]} onClick={handleSampleClick} />
-            : <div />} */}
+            : <div />}
         </div>
       </div>
       <div className="bottom-sp">
@@ -17,8 +81,7 @@ const SplitScreenSp = () => {
         <div className="comparison-container-sp">
           {props.trials.length && sClick === 1 ?
             <>
-              <img alt={props.trials[i][1]} src={props.trials[i][2]} className={props.trials[i][3]} value={props.trials[i][4]} onClick={handleComparisonClick} />
-              <img alt={props.trials[i][5]} src={props.trials[i][6]} className={props.trials[i][7]} value={props.trials[i][8]} onClick={handleComparisonClick} />
+              <img alt={props.trials[i][3]} src={props.trials[i][1]} className="comparison" value={props.trials[i][2]} onClick={handleComparisonClick} />
             </>
             :
             <div />
