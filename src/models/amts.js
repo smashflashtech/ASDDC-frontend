2b7e3ea933a0d98ca33fdfbfdb7128650027c441
smashflashtech <smash.flash.tech@gmail.com>
2020-12-16 10:54:16 -0800

    adds create method

--- src/models/amts.js
+++ src/models/amts.js
@@ -6,6 +6,17 @@ class AmtsModel {
     return fetch(`${url}/amts`).then(res => res.json())
   }
 
+  static create = (trialcode, responseData) => {
+    return fetch(`${url}/response/${trialcode}`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(responseData)
+    })
+    .then(res => res.json())
+  }
+
 }
 
 export default AmtsModel
