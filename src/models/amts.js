ee85eb10c85cb805f2f887517ef08a70be6ddeae
smashflashtech <smash.flash.tech@gmail.com>
2020-12-16 11:38:54 -0800

    removes create method

--- src/models/amts.js
+++ src/models/amts.js
@@ -5,18 +5,6 @@ class AmtsModel {
   static stimuli = () => {
     return fetch(`${url}/amts`).then(res => res.json())
   }
-
-  static create = (trialcode, responseData) => {
-    return fetch(`${url}/response/${trialcode}`, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json"
-      },
-      body: JSON.stringify(responseData)
-    })
-    .then(res => res.json())
-  }
-
 }
 
 export default AmtsModel
