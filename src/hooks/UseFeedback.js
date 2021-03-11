8abcc981719f757cee6a4784ee67ada3fd729cc6
smashflashtech <smash.flash.tech@gmail.com>
2021-03-10 23:35:38 -0800

    changes ITI to 2-s and clears Timeout

--- src/hooks/UseFeedback.js
+++ src/hooks/UseFeedback.js
@@ -1,20 +1,19 @@
 import React from 'react'
 
 const UseFeedback = (selectedValue) => {
-  // const feedback = (selectedValue) => {
   let feedback = localStorage.getItem('feedback')
   if (feedback === "false") {
     let overlay = document.getElementById('neutral')
     overlay.style.display = "inline-block"
-    setTimeout(function () { overlay.style.display = "none" }, 3000)
+    let timerId = setTimeout(function () { overlay.style.display = "none"; clearTimeout(timerId) }, 2000)
   } else if (feedback === "true" && selectedValue === "correct") {
     let overlay = document.getElementById('yay')
     overlay.style.display = "inline-block"
-    setTimeout(function () { overlay.style.display = "none" }, 3000)
+    let timerId = setTimeout(function () { overlay.style.display = "none"; clearTimeout(timerId) }, 2000)
   } else if (feedback === "true" && selectedValue === "incorrect") {
     let overlay = document.getElementById('nay')
     overlay.style.display = "inline-block"
-    setTimeout(function () { overlay.style.display = "none" }, 3000)
+    let timerId = setTimeout(function () { overlay.style.display = "none"; clearTimeout(timerId) }, 2000)
   }
   // }
 
